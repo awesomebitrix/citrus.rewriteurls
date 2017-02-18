b1058241629884a74f22332a65e97afc2da6f51a
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-18 13:31:32 +0300

    fix

---
+++ install/version.php
@@ -0,0 +1,8 @@
+<?php
+
+// NOTE this file must compatible with php 5.3
+
+$arModuleVersion = array(
+	'VERSION' 		=> '2017.02.18',
+	'VERSION_DATE' 	=> '2017-02-18'
+);
---
+++ lang/ru/install/index.php
@@ -0,0 +1,4 @@
+<?php
+
+$MESS["CITRUS_REWRITEURLS_MODULE_NAME"] = "Реврайт и замена url для SEO";
+$MESS["CITRUS_REWRITEURLS_MODULE_DESCRIPTION"] = "Реврайт и динамическая замена url на страницах сайта";
