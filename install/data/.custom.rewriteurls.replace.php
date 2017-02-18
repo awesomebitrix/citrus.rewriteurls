5dd745aaeac107b08c094f936c805fef80cbd98a
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-18 13:44:33 +0300

    fix

--- install/data/.custom.rewriteurls.replace.php
+++ install/data/.custom.rewriteurls.replace.php
@@ -1,5 +1,10 @@
 <?php
 
 return array(
-	"/test/" => "/contacts/",
+	"/kontakti/" => "/contacts/index.php",
+	"/novosti/" => "/news/index.php",
+	"/uslugi/" => "/services/index.php",
+	"/catalog/" => "/products/index.php",
+	"/catalog/ofisnya-mebel/" => "/products/2/",
+	"/catalog/mebel-dlya-kuhni/" => "/products/3/",
 );
