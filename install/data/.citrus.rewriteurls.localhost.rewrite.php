917b7d114f4953f56f567e6f4bb79dc27485c461
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-24 13:24:11 +0300

    fix

--- install/data/.citrus.rewriteurls.localhost.rewrite.php
+++ install/data/.citrus.rewriteurls.localhost.rewrite.php
@@ -2,6 +2,4 @@
 
 return array(
 	"/kontakti/" => "/contacts/",
-	"/novosti/" => "/news/",
-	"/uslugi/" => "/services/",
 );
--- install/data/.citrus.rewriteurls.localhost.rewriteparts.php
+++ install/data/.citrus.rewriteurls.localhost.rewriteparts.php
@@ -4,4 +4,6 @@ return array(
 	"/catalog/ofisnya-mebel/" => "/products/2/",
 	"/catalog/mebel-dlya-kuhni/" => "/products/3/",
 	"/catalog/" => "/products/",
+	"/novosti/" => "/news/",
+	"/uslugi/" => "/services/",
 );
